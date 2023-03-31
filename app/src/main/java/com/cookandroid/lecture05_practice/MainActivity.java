a32e672a46eaebcfe94b44fcdb597049994f0f3a
DJLim_N <djlim1338@gmail.com>
2023-03-31 12:15:09 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/lecture05_practice/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cookandroid.lecture05_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mainBtnPrac4, mainBtnPrac5, mainBtnPrac6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mainBtnPrac4 = (Button) findViewById(R.id.mainBtnPrac4);
+        mainBtnPrac5 = (Button) findViewById(R.id.mainBtnPrac5);
+        mainBtnPrac6 = (Button) findViewById(R.id.mainBtnPrac6);
+
+        mainBtnPrac4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Prac4.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
